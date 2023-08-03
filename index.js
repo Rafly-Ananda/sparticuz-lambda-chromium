26d23ec473acb227f729a34bd748d1f048d54951
rafly-ananda <anandarafly15@gmail.com>
2023-08-04 02:11:52 +0700

    attempting to fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require("express")();
+const axios = require("axios");
 
 let chrome = {};
 let puppeteer;
@@ -13,6 +14,14 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
 app.get("/api", async (req, res) => {
   let options = {};
 
+  if (!req.query.bookingId) {
+    throw "Need bookingId parameter";
+  }
+
+  if (!req.query.url) {
+    throw "Need query parameter";
+  }
+
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     options = {
       args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
@@ -25,12 +34,21 @@ app.get("/api", async (req, res) => {
 
   try {
     let browser = await puppeteer.launch(options);
-
     let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
+    await page.goto(req.query.url, {
+      waitUntil: "networkidle0",
+    });
+    const buffer = await page.pdf({ format: "a4" });
+    console.log("PDF Saved");
+    res
+      .send({
+        message: "PDF Generated",
+        s3Url: `https://gadjah-ticketing-platform.s3.ap-southeast-1.amazonaws.com/${req.query.bookingId}.pdf`,
+      })
+      .status(200);
   } catch (err) {
-    console.error(err);
+    console.log(err);
+    res.send({ message: "Request Failed", error: err.message });
     return null;
   }
 });
